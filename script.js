01ffe73c0cf488dd80fba6d46e4f0c0140937f78
bloodyhill <87740931+bloodyhill@users.noreply.github.com>
2023-07-30 09:37:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,14 @@
 $(document).ready(function() {
-    $('.choice').click(function() {
-        var target = $(this).data('target');
-        var answer = $(this).data('answer');
-
-        if (target) {
-            $(this).closest('.question').hide();
-            $('#' + target).show(400);
-        } else if (answer) {
-            $('#answer-text').text(answer);
-            $(this).closest('.question').hide();
-            $('.answer').show(400);
-            $('#start-over').css("display", "block");
-        }
-    });
-
-    $('#start-over').click(function() {
-        $('.question').hide();
-        $('#q1').show();
-        $('.answer').hide();
-        $(this).hide();
-    });
-$(document).ready(function() {
+    // Handle change event for dropdown menu
     $('#choice1').change(function() {
         var target = $(this).val();
-
         if (target) {
             $('.question').not('#' + target).hide();
             $('#' + target).fadeIn(400);
         }
     });
 
+    // Handle click event for choice buttons
     $('.choice').click(function() {
         var target = $(this).data('target');
         var answer = $(this).data('answer');
@@ -45,6 +24,7 @@ $(document).ready(function() {
         }
     });
 
+    // Handle click event for start-over button
     $('#start-over').click(function() {
         $('.question').hide();
         $('#q1').show();

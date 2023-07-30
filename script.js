43474eed2e567d3eeb3323158a3634f2ce6eea42
bloodyhill <87740931+bloodyhill@users.noreply.github.com>
2023-07-30 09:29:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -20,16 +20,35 @@ $(document).ready(function() {
         $('.answer').hide();
         $(this).hide();
     });
-});
-
 $(document).ready(function() {
     $('#choice1').change(function() {
         var target = $(this).val();
+
         if (target) {
-            $(this).closest('.question').hide();
+            $('.question').not('#' + target).hide();
             $('#' + target).fadeIn(400);
         }
     });
 
-    // Other event handlers
+    $('.choice').click(function() {
+        var target = $(this).data('target');
+        var answer = $(this).data('answer');
+
+        if (target) {
+            $(this).closest('.question').hide();
+            $('#' + target).show(400);
+        } else if (answer) {
+            $('#answer-text').text(answer);
+            $(this).closest('.question').hide();
+            $('.answer').show(400);
+            $('#start-over').css("display", "block");
+        }
+    });
+
+    $('#start-over').click(function() {
+        $('.question').hide();
+        $('#q1').show();
+        $('.answer').hide();
+        $(this).hide();
+    });
 });
